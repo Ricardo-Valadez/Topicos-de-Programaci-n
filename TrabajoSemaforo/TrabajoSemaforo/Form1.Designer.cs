723252d8f522d9c5e43175414d031e32cf7c9970
Ricardo Valadez <99301599+Ricardo-Valadez@users.noreply.github.com>
2022-10-07 15:21:50 -0700

    Add files via upload

---
+++ TrabajoSemaforo/TrabajoSemaforo/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace TrabajoSemaforo
+{
+    partial class Semaforo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Semaforo));
+            this.ptbImagen = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEncender = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ptbImagen2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImagen)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImagen2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ptbImagen
+            // 
+            this.ptbImagen.Image = global::TrabajoSemaforo.Properties.Resources.Apagado;
+            this.ptbImagen.Location = new System.Drawing.Point(471, 115);
+            this.ptbImagen.Name = "ptbImagen";
+            this.ptbImagen.Size = new System.Drawing.Size(267, 287);
+            this.ptbImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbImagen.TabIndex = 0;
+            this.ptbImagen.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(617, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Condición";
+            // 
+            // btnEncender
+            // 
+            this.btnEncender.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(49)))), ((int)(((byte)(50)))));
+            this.btnEncender.Font = new System.Drawing.Font("Lucida Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncender.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnEncender.Location = new System.Drawing.Point(6, 28);
+            this.btnEncender.Name = "btnEncender";
+            this.btnEncender.Size = new System.Drawing.Size(131, 73);
+            this.btnEncender.TabIndex = 2;
+            this.btnEncender.Text = "Encender";
+            this.btnEncender.UseVisualStyleBackColor = false;
+            this.btnEncender.Click += new System.EventHandler(this.btnEncender_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(49)))), ((int)(((byte)(50)))));
+            this.btnApagar.Font = new System.Drawing.Font("Lucida Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnApagar.Location = new System.Drawing.Point(6, 186);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(131, 73);
+            this.btnApagar.TabIndex = 3;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = false;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(49)))), ((int)(((byte)(50)))));
+            this.btnDetener.Font = new System.Drawing.Font("Lucida Sans", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetener.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnDetener.Location = new System.Drawing.Point(6, 107);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(131, 73);
+            this.btnDetener.TabIndex = 4;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = false;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.groupBox1.Controls.Add(this.btnDetener);
+            this.groupBox1.Controls.Add(this.btnEncender);
+            this.groupBox1.Controls.Add(this.btnApagar);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(322, 115);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(143, 287);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // ptbImagen2
+            // 
+            this.ptbImagen2.Image = global::TrabajoSemaforo.Properties.Resources.Apagado;
+            this.ptbImagen2.Location = new System.Drawing.Point(49, 115);
+            this.ptbImagen2.Name = "ptbImagen2";
+            this.ptbImagen2.Size = new System.Drawing.Size(267, 287);
+            this.ptbImagen2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbImagen2.TabIndex = 6;
+            this.ptbImagen2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(49)))), ((int)(((byte)(50)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-2, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(806, 75);
+            this.panel1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(14, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(352, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Simulación: Semaforo";
+            // 
+            // Semaforo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(144)))), ((int)(((byte)(175)))), ((int)(((byte)(197)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ptbImagen2);
+            this.Controls.Add(this.ptbImagen);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Semaforo";
+            this.Text = "Trabajo Semaforo";
+            this.Load += new System.EventHandler(this.TrabajoSemaforo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImagen)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImagen2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptbImagen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEncender;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox ptbImagen2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TrabajoSemaforo/TrabajoSemaforo/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoSemaforo
+{
+    public partial class Semaforo : Form
+    {
+        public Semaforo()
+        {
+            InitializeComponent();
+        }
+
+        int caso = 0;
+
+        private void TrabajoSemaforo_Load(object sender, EventArgs e)
+        {
+            //Aqui elige la imagen que ocupamos
+            ptbImagen.Image = TrabajoSemaforo.Properties.Resources.Apagado;
+            ptbImagen2.Image = TrabajoSemaforo.Properties.Resources.Apagado;
+
+            //Botones que no se necesiten estan desabilitados
+
+            btnApagar.Enabled = false;
+            btnDetener.Enabled = false;
+        }
+
+        private void btnEncender_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+            btnApagar.Enabled = true;
+            btnDetener.Enabled = true;
+            btnEncender.Enabled = false;
+
+            label1.Text = "Encendido";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (caso)
+            {
+                case 0:
+
+                    ptbImagen.Image = TrabajoSemaforo.Properties.Resources.Verde;
+                    ptbImagen2.Image = TrabajoSemaforo.Properties.Resources.Rojo;
+
+                    timer1.Interval = 5000;
+
+                    caso = 1;
+                    break;
+
+                case 1:
+                    ptbImagen.Image = TrabajoSemaforo.Properties.Resources.Amarillo;
+                    ptbImagen2.Image = TrabajoSemaforo.Properties.Resources.Amarillo;
+
+                    timer1.Interval = 2000;
+
+                    caso = 2;
+                    break;
+
+                case 2:
+                    ptbImagen.Image = TrabajoSemaforo.Properties.Resources.Rojo;
+                    ptbImagen2.Image = TrabajoSemaforo.Properties.Resources.Verde;
+
+                    timer1.Interval = 5000;
+
+                    caso = 0;
+                    break;
+            }
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            if (btnDetener.Text == "Detener")
+            {
+                timer1.Enabled = false;
+
+                btnDetener.Text = "Reanudar";
+
+                label1.Text = "Detenido";
+            }
+            else if(btnDetener.Text=="Reanudar")
+            {
+                timer1.Enabled = true;
+
+                btnDetener.Text = "Detener";
+
+                label1.Text = "Encendido";
+            }
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            btnDetener.Enabled = false;
+            btnApagar.Enabled = false;
+
+            btnEncender.Enabled = true;
+
+            ptbImagen.Image = TrabajoSemaforo.Properties.Resources.Apagado;
+            ptbImagen2.Image = TrabajoSemaforo.Properties.Resources.Apagado;
+
+            label1.Text = "Apagado";
+        }
+    }
+}
